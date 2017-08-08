b70b22aef4db179d4a881c17d86a11d2c72aedf0
Knot <Knot.kunai@gmail.com>
2017-08-08 11:22:13 +0700

    Add tab adapter Now tablayout  working well

--- app/src/main/java/com/minipg/fanster/armoury/activity/MainActivity.java
+++ app/src/main/java/com/minipg/fanster/armoury/activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.minipg.fanster.armoury.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.crashlytics.android.Crashlytics;
 import com.minipg.fanster.armoury.R;
+import com.minipg.fanster.armoury.adapter.PageAdapter;
 import com.minipg.fanster.armoury.fragment.MainFragment;
 import com.minipg.fanster.armoury.fragment.TabProfileFragment;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
+    private ViewPager viewPager;
 
 
     @Override
@@ -42,16 +45,19 @@ public class MainActivity extends AppCompatActivity {
     private void initInstance() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
         createTabIcons(tabLayout);
-
-
     }
 
     private void createTabIcons(TabLayout tabLayout) {
-        tabLayout.addTab(tabLayout.newTab().setText("Profile").setIcon(R.mipmap.ic_profile));
-        tabLayout.addTab(tabLayout.newTab().setText("Library").setIcon(R.mipmap.ic_library_white));
-        tabLayout.addTab(tabLayout.newTab().setText("Popular").setIcon(R.mipmap.ic_rating));
-        tabLayout.addTab(tabLayout.newTab().setText("Loved").setIcon(R.mipmap.ic_faverite));
+        tabLayout.getTabAt(0).setText(R.string.tab_profile_title).setIcon(R.mipmap.ic_profile);
+        tabLayout.getTabAt(1).setText(R.string.tab_library_title).setIcon(R.mipmap.ic_library_white);
+        tabLayout.getTabAt(2).setText(R.string.tab_popular_title).setIcon(R.mipmap.ic_rating);
+        tabLayout.getTabAt(3).setText(R.string.tab_liked_title).setIcon(R.mipmap.ic_faverite);
     }
 }
---
+++ app/src/main/java/com/minipg/fanster/armoury/adapter/PageAdapter.java
@@ -0,0 +1,50 @@
+package com.minipg.fanster.armoury.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.minipg.fanster.armoury.R;
+import com.minipg.fanster.armoury.fragment.TabKnowlegdeFragment;
+import com.minipg.fanster.armoury.fragment.TabLikedFragment;
+import com.minipg.fanster.armoury.fragment.TabPopularFragment;
+import com.minipg.fanster.armoury.fragment.TabProfileFragment;
+
+/**
+ * Created by Knot on 8/8/2017.
+ */
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new TabProfileFragment();
+            case 1: return new TabKnowlegdeFragment();
+            case 2: return new TabPopularFragment();
+            case 3: return new TabLikedFragment();
+            default: return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0: return "Profile";
+            case 1: return "Library";
+            case 2: return "Popular";
+            case 3: return "Liked";
+            default: return null;
+        }
+    }
+
+}
